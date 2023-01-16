da790de785dd070839ac8f9f37e1c102db454750
Dylan Lukes <lukes.dylan@gmail.com>
2023-01-16 02:05:18 -0800

    use method references for clarity

--- app/src/main/java/edu/ucsd/cse110/lab2/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab2/MainActivity.java
@@ -51,87 +51,93 @@ public class MainActivity extends AppCompatActivity {
         btnTimes = findViewById(R.id.btn_times);
         btnEquals = findViewById(R.id.btn_equals);
 
-        // Register the numerical buttons to modify the display.
-        // Note: we _can't_ use var here because Java needs to know what class to interpret the
-        // special lambda syntax as shorthand for. Refer back to the link in Lab 1 if unsure.
-        View.OnClickListener onNumBtnClicked = (View view) -> {
-            // Before casting, it's best to be sure we actually have what we think!
-            assert view instanceof Button;
-            Button btn = (Button) view;
-
-            // Retrieve the text from the button, and append it to the display.
-            var num = btn.getText();
-            if (displayStr.equals("0")) {
-                // If the display is just 0, set it to the digit entered.
-                displayStr = num.toString();
-            } else {
-                // Otherwise, append the next digit.
-                displayStr += num;
-            }
-            updateDisplay();
-        };
+        // Register the number button click handler.
         for (var numBtn : numBtns) {
-            numBtn.setOnClickListener(onNumBtnClicked);
+            numBtn.setOnClickListener(this::onNumberButtonClicked);
         }
-
-        // Register the dot button.
-        btnDot.setOnClickListener((View view) -> {
-            assert view instanceof Button;
-            Button btn = (Button) view;
-
-            // If we already have a decimal, don't do anything.
-            if (displayStr.contains(".")) return;
-            displayStr += ".";
-            updateDisplay();
-        });
 
         // Register the operation (plus, times) buttons.
-        View.OnClickListener onOpBtnClicked = (View view) -> {
-            assert view instanceof Button;
-            Button btn = (Button) view;
-
-            // Parse the current number and save it to "previous value".
-            storedValue = new BigDecimal(displayStr);
-            clearDisplay();
-
-            // Set the pending operation.
-            pendingOp = Optional.of(btn.getText().toString());
-        };
         for (var opBtn : new Button[] {btnPlus, btnTimes}) {
-            opBtn.setOnClickListener(onOpBtnClicked);
+            opBtn.setOnClickListener(this::onOperationButtonClicked);
         }
 
+        // Register the dot button click handler.
+        btnDot.setOnClickListener(this::onDotButtonClicked);
+
         // Register the clear button.
-        btnClear.setOnClickListener((View view) -> {
-            clearDisplay();
-        });
+        btnClear.setOnClickListener(this::onClearButtonClicked);
 
         // Register the equals button (this is where the magic happens!)
-        btnEquals.setOnClickListener((View view) -> {
-            // Parse the current value.
-            var currValue = new BigDecimal(displayStr);
+        btnEquals.setOnClickListener(this::onEqualsButtonClicked);
+    }
 
-            // Compute the result.
-            // The map method calls the given lambda with the value inside
-            // the Optional if it exists (is not empty). The orElse provides
-            // a fallback value if there is no operation. In this case, it
-            // is whatever is already on the screen.
-            BigDecimal result = pendingOp.map((opStr) -> {
-                if (opStr.equals("+")) {
-                    return storedValue.add(currValue);
-                }
-                if (opStr.equals("×")) {
-                    return storedValue.multiply(currValue);
-                }
-                throw new RuntimeException("Unknown operation: " + opStr);
-            }).orElse(currValue);
+    void onNumberButtonClicked(View view) {
+        // Before casting, it's best to be sure we actually have what we think!
+        assert view instanceof Button;
+        Button btn = (Button) view;
 
-            // Clear the pending operation and update the display.
-            pendingOp = Optional.empty();
-            displayStr = String.valueOf(result);
-            trimDisplayStr();
-            updateDisplay();
-        });
+        // Retrieve the text from the button, and append it to the display.
+        var num = btn.getText();
+        if (displayStr.equals("0")) {
+            // If the display is just 0, set it to the digit entered.
+            displayStr = num.toString();
+        } else {
+            // Otherwise, append the next digit.
+            displayStr += num;
+        }
+        updateDisplay();
+    }
+
+    void onDotButtonClicked(View view) {
+        assert view instanceof Button;
+        Button btn = (Button) view;
+
+        // If we already have a decimal, don't do anything.
+        if (displayStr.contains(".")) return;
+        displayStr += ".";
+        updateDisplay();
+    }
+
+    void onOperationButtonClicked(View view) {
+        assert view instanceof Button;
+        Button btn = (Button) view;
+
+        // Parse the current number and save it to "previous value".
+        storedValue = new BigDecimal(displayStr);
+        clearDisplay();
+
+        // Set the pending operation.
+        pendingOp = Optional.of(btn.getText().toString());
+    }
+
+    void onClearButtonClicked(View view) {
+        clearDisplay();
+    }
+
+    void onEqualsButtonClicked(View view) {
+        // Parse the current value.
+        var currValue = new BigDecimal(displayStr);
+
+        // Compute the result.
+        // The map method calls the given lambda with the value inside
+        // the Optional if it exists (is not empty). The orElse provides
+        // a fallback value if there is no operation. In this case, it
+        // is whatever is already on the screen.
+        BigDecimal result = pendingOp.map((opStr) -> {
+            if (opStr.equals("+")) {
+                return storedValue.add(currValue);
+            }
+            if (opStr.equals("×")) {
+                return storedValue.multiply(currValue);
+            }
+            throw new RuntimeException("Unknown operation: " + opStr);
+        }).orElse(currValue);
+
+        // Clear the pending operation and update the display.
+        pendingOp = Optional.empty();
+        displayStr = String.valueOf(result);
+        trimDisplayStr();
+        updateDisplay();
     }
 
     void updateDisplay() {
