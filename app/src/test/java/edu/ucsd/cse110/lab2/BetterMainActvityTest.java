ce16f5860d04047e255a4af737e0594258f0bd17
incisors <haj003@ucsd.edu>
2023-01-18 13:48:21 -0800

    added

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActvityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActvityTest.java
@@ -1,4 +1,36 @@
 package edu.ucsd.cse110.lab2;
 
+import static org.junit.Assert.assertEquals;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class BetterMainActvityTest {
+    @Test
+    public void test_one_plus_one() {
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.onActivity(activity -> {
+            Button bt1 = (Button)activity.findViewById(R.id.btn_one);
+            Button plus = (Button)activity.findViewById(R.id.btn_plus);
+            Button equal = (Button)activity.findViewById(R.id.btn_equals);
+            TextView display = (TextView)activity.findViewById(R.id.display);
+            bt1.performClick();
+            plus.performClick();
+            bt1.performClick();
+            equal.performClick();
+            assertEquals(display.getText(),"2");
+
+        });
+
+    }
 }
